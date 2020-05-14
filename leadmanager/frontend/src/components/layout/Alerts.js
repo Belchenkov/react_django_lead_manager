1488f19547ac3da5470861e635d1b2624772a350
Aleksey Belchenkov <u608110@gmail.com>
2020-05-14 16:06:04 +0300

    Bring Alert Plugin

---
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+import { withAlert } from "react-alert";
+
+export class Alerts extends Component {
+    componentDidMount() {
+        console.log(this.props)
+        //this.props.alert.show('It works!');
+    }
+
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default withAlert(Alerts);
